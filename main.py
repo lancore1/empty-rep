46fd399e3ac513d708579df84b58d75723e4dd64
lancore1 <kravastok13345@gmail.com>
2025-11-03 20:16:29 +0200

    add function that counts letter

--- main.py
+++ main.py
@@ -17,3 +17,12 @@ def union_set(set1:set,set2:set) -> set:
 #func of unique list
 def unique_list(list1:list) -> list:
     return list(set(list1))
+
+
+#func function that counts letters
+def count_char_in_text(text:str, enter_char: str)  -> int :
+    counter = 0
+    for char in text:
+        if char == enter_char:
+            counter += 1
+    return counter
